a491a26c88aef986830c3c52909670c9572ad11e
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2025-05-20 14:15:29 +0100

    More work on the topics page.

--- plubPulseApp/src/pages/Topics.jsx
+++ plubPulseApp/src/pages/Topics.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
-import {FaBook} from "react-icons/fa";
 import {useNavigate} from "react-router-dom";
+import { FaBook, FaHeartbeat, FaBrain, FaBalanceScale, FaFlask, FaPills, FaUserMd } from "react-icons/fa";
 
 
 const Topics = () => {
@@ -15,106 +15,166 @@ const Topics = () => {
 
     return (
         <div className="container topics-container">
-            <div className="row mt-5">
-                <h2>Medical Specialties</h2>
-                <p>Choose a specialty to focus your exam preparation</p>
+            <div className="row mt-5 text-center mb-2">
+                <h2 className="display-5 fw-semibold">Medical Specialties</h2>
+                <p className="lead mx-auto" style={{maxWidth: "800px"}}>Choose a specialty to focus your exam preparation</p>
             </div>
             <div className="row mb-5">
                 <div className="row g-4">
                     {/* Cardiology Card */}
                     <div className="col-md-4">
-                        <div className="topic-card-square card rounded-4 p-4 border-0 shadow-sm mb-4">
-                            <h3 className="card-title fs-4 mb-3">Cardiology</h3>
-                            <p className="card-text text-muted mb-4">
-                                Test your knowledge in Cardiology with focused questions.
+                        <div className="topic-card-square card rounded-4 p-4 border-0 shadow mb-4">
+                            <div className="card-title fs-4 d-flex mb-3">
+                                <h3 className="card-title fs-4">Cardiology</h3>
+                                <FaHeartbeat className="ms-2 text-danger mt-1"/>
+                            </div>
+                            <p className="card-text text-muted mb-4 mt-2">
+                                Topics covered:
                             </p>
+                            <ul>
+                                <li>Valvular Heart Disease</li>
+                                <li>Heart Failure</li>
+                                <li>Electrocardiography</li>
+                                <li>Cardiac Emergencies</li>
+                                <li>Anticoagulation</li>
+                            </ul>
                             <button
-                                className="text-black w-100 w-100 p-2 d-flex align-items-center justify-content-center topic-button"
+                                className="topic-button w-100 p-2 d-flex align-items-center justify-content-center"
                                 onClick={() => handleStartQuiz("Cardiology")}
                             >
-                                <FaBook className="me-2"/> Start Cardiology Quiz
+                                <FaHeartbeat className="me-2"/> Start Cardiology Quiz
                             </button>
                         </div>
                     </div>
                     {/* Neurology Card */}
                     <div className="col-md-4">
-                        <div className="topic-card-square card rounded-4 p-4 border-0 shadow-sm mb-4">
-                            <h3 className="card-title fs-4 mb-3">Neurology </h3>
-                            <p className="card-text text-muted mb-4">
-                                Test your knowledge in Neurology with focused questions.
+                        <div className="topic-card-square card rounded-4 p-4 border-0 shadow mb-4">
+                            <div className="card-title fs-4 d-flex mb-3">
+                                <h3 className="card-title fs-4">Neurology</h3>
+                                <FaBrain className="ms-2 text-danger mt-1"/>
+                            </div>
+                            <p className="card-text text-muted mb-4 mt-2">
+                                Topics covered:
                             </p>
+                            <ul>
+                                <li>Stroke and TIA</li>
+                                <li>Seizure Disorders</li>
+                                <li>Multiple Sclerosis</li>
+                                <li>Cranial Nerves</li>
+                                <li>Movement Disorders</li>
+                            </ul>
                             <button
-                                className="text-black w-100 p-2 p-2 d-flex align-items-center justify-content-center topic-button"
+                                className="topic-button w-100 p-2 d-flex align-items-center justify-content-center"
                                 onClick={() => handleStartQuiz("Neurology")}
                             >
-                                <FaBook className="me-2"/> Start Neurology Quiz
+                                <FaBrain className="me-2"/> Start Neurology Quiz
                             </button>
                         </div>
                     </div>
-                    {/* Ethics  Card */}
+                    {/* Ethics Card */}
                     <div className="col-md-4">
-                        <div className="topic-card-square card rounded-4 p-4 border-0 shadow-sm mb-4">
-                            <h3 className="card-title fs-4 mb-3">Ethics </h3>
-                            <p className="card-text text-muted mb-4">
-                                Test your knowledge in Ethics specialty with focused questions.
+                        <div className="topic-card-square card rounded-4 p-4 border-0 shadow mb-4">
+                            <div className="card-title fs-4 d-flex mb-3">
+                                <h3 className="card-title fs-4">Ethics</h3>
+                                <FaBalanceScale className="ms-2 text-success mt-1"/>
+                            </div>
+                            <p className="card-text text-muted mb-4 mt-2">
+                                Topics covered:
                             </p>
+                            <ul>
+                                <li>Autonomy and Consent</li>
+                                <li>Justice and Resource Allocation</li>
+                                <li>Beneficence and Nonmaleficence</li>
+                                <li>Veracity and Confidentiality</li>
+                                <li>Professional Conduct</li>
+                            </ul>
                             <button
-                                className="text-black w-100 p-2  p-2 d-flex align-items-center justify-content-center topic-button"
+                                className="topic-button w-100 p-2 d-flex align-items-center justify-content-center"
                                 onClick={() => handleStartQuiz("Ethics")}
                             >
-                                <FaBook className="me-2"/> Start Ethics Quiz
+                                <FaBalanceScale className="me-2"/> Start Ethics Quiz
                             </button>
                         </div>
                     </div>
                     {/* Endocrinology Card */}
                     <div className="col-md-4">
-                        <div className="topic-card-square card rounded-4 p-4 border-0 shadow-sm mb-4">
-                            <h3 className="card-title fs-4 mb-3">Endocrinology </h3>
-                            <p className="card-text text-muted mb-4">
-                                Test your knowledge in Endocrinology with focused questions.
+                        <div className="topic-card-square card rounded-4 p-4 border-0 shadow mb-4">
+                            <div className="card-title fs-4 d-flex mb-3">
+                                <h3 className="card-title fs-4">Endocrinology</h3>
+                                <FaFlask className="ms-2 text-warning mt-1"/>
+                            </div>
+                            <p className="card-text text-muted mb-4 mt-2">
+                                Topics covered:
                             </p>
+                            <ul>
+                                <li>Thyroid Disorders</li>
+                                <li>Diabetes Mellitus</li>
+                                <li>Pituitary Function</li>
+                                <li>Adrenal Disorders</li>
+                                <li>Calcium Homeostasis</li>
+                            </ul>
                             <button
-                                className="text-black w-100 p-2 d-flex align-items-center justify-content-center topic-button"
-                                onClick={() => handleStartQuiz("Endocrinology ")}
+                                className="topic-button w-100 p-2 d-flex align-items-center justify-content-center"
+                                onClick={() => handleStartQuiz("Endocrinology")}
                             >
-                                <FaBook className="me-2"/> Start Endocrinology Quiz
+                                <FaFlask className="me-2"/> Start Endocrinology Quiz
                             </button>
                         </div>
                     </div>
                     {/* Pharmacology Card */}
                     <div className="col-md-4">
-                        <div className="topic-card-square card rounded-4 p-4 border-0 shadow-sm mb-4">
-                            <h3 className="card-title fs-4 mb-3">Pharmacology </h3>
-                            <p className="card-text text-muted mb-4">
-                                Test your knowledge in Pharmacology with focused questions.
+                        <div className="topic-card-square card rounded-4 p-4 border-0 shadow mb-4">
+                            <div className="card-title fs-4 d-flex mb-3">
+                                <h3 className="card-title fs-4">Pharmacology</h3>
+                                <FaPills className="ms-2 text-info mt-1"/>
+                            </div>
+                            <p className="card-text text-muted mb-4 mt-2">
+                                Topics covered:
                             </p>
+                            <ul>
+                                <li>Antimicrobial Drugs</li>
+                                <li>Cardiovascular Medications</li>
+                                <li>Antidepressants and Anxiolytics</li>
+                                <li>NSAIDs and Analgesics</li>
+                                <li>Drug Toxicity and Antidotes</li>
+                            </ul>
                             <button
-                                className="text-black w-100 p-2 d-flex align-items-center justify-content-center topic-button"
-                                onClick={() => handleStartQuiz("Endocrinology ")}
+                                className="topic-button w-100 p-2 d-flex align-items-center justify-content-center"
+                                onClick={() => handleStartQuiz("Pharmacology")}
                             >
-                                <FaBook className="me-2"/> Start Endocrinology Quiz
+                                <FaPills className="me-2"/> Start Pharmacology Quiz
                             </button>
                         </div>
                     </div>
                     {/* Psychiatry Card */}
                     <div className="col-md-4">
-                        <div className="topic-card-square card rounded-4 p-4 border-0 shadow-sm mb-4">
-                            <h3 className="card-title fs-4 mb-3">Psychiatry </h3>
-                            <p className="card-text text-muted mb-4">
-                                Test your knowledge in Psychiatry with focused questions.
+                        <div className="topic-card-square card rounded-4 p-4 border-0 shadow mb-4">
+                            <div className="card-title fs-4 d-flex mb-3">
+                                <h3 className="card-title fs-4">Psychiatry</h3>
+                                <FaUserMd className="ms-2 text-secondary mt-1"/>
+                            </div>
+                            <p className="card-text text-muted mb-4 mt-2">
+                                Topics covered:
                             </p>
+                            <ul>
+                                <li>Mood Disorders</li>
+                                <li>Schizophrenia</li>
+                                <li>Anxiety Disorders</li>
+                                <li>Psychopharmacology</li>
+                                <li>ADHD and Developmental Disorders</li>
+                            </ul>
                             <button
-                                className="text-black w-100 p-2 d-flex align-items-center justify-content-center topic-button"
-                                onClick={() => handleStartQuiz("Psychiatry ")}
+                                className="topic-button w-100 p-2 d-flex align-items-center justify-content-center"
+                                onClick={() => handleStartQuiz("Psychiatry")}
                             >
-                                <FaBook className="me-2"/> Start Psychiatry Quiz
+                                <FaUserMd className="me-2"/> Start Psychiatry Quiz
                             </button>
                         </div>
                     </div>
                 </div>
             </div>
-</div>
-)
+        </div>
+    )
 }
 
 export default Topics;
